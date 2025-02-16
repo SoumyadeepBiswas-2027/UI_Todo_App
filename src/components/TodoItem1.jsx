a7646e067db7857adf75bbfa2d4329f0e0a4ac88
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-02-17 02:32:19 +0530

    CSS added(improved_UI)

--- src/components/TodoItem1.jsx
+++ src/components/TodoItem1.jsx
@@ -1,16 +1,16 @@
 function TodoItem1() {
 
-  let todoName='Buy Cake!';
-  let todoDate='3/03/2024';
+  let todoName="Buy Cake!";
+  let todoDate="3/03/2024";
 
 
   return (
-    <div class="container text-center">
+    <div class="container ">
   
-    <div class="row">
+    <div className="row kg-row">
       <div class="col-6">{todoName}</div>
         <div class="col-4">{todoDate}</div>
-        <div class="col-2"><button type="button" class="btn btn-danger">Delete</button>
+        <div class="col-2"><button type="button" class="btn btn-danger kg-button">Delete</button>
         </div>
      </div>
      </div>
